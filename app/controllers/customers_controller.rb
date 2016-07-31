8653f3828fab5c15b6e55f733b6a23ae7c29d008
novneetnov <novneet346iith@gmail.com>
2016-07-31 16:29:36 +0530

    Add pagination to search results

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,13 @@
 class CustomersController < ApplicationController
+	PAGE_SIZE = 10
   def index
+		@page = (params[:page] || 0).to_i
 		if params[:keywords].present?
 			customer_search_term = CustomSearchTerm.new(params[:keywords])
-			@customers = Customer.where(customer_search_term.where_clause, customer_search_term.where_args).order(customer_search_term.order)
+			@customers = Customer.where(customer_search_term.where_clause, customer_search_term.where_args)
+													 .order(customer_search_term.order)
+													 .offset(PAGE_SIZE * @page)
+													 .limit(PAGE_SIZE)
 		else
 			@customers = []
 		end	
